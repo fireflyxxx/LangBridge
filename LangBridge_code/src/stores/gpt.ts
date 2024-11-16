2e69573886b7a01481b1b4d8c7fb0e73d9986ef3
firefly <fireflyx26710@gmail.com>
2024-11-16 15:54:36 +0800

    Delete LangBridge_code directory

--- LangBridge_code/src/stores/gpt.ts
+++
@@ -1,8 +0,0 @@
-import { localStorageWritable } from '$helpers/store';
-
-export enum GPT {
-	'tongyi' = 'qwen-vl-max',
-	'four' = 'gpt-4'
-}
-
-export const gpt = localStorageWritable<GPT>('openai-gpt-version', GPT['tongyi']);
